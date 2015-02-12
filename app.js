d5dd98c59bad3fc2d8fcab0ad149d61cba3585aa
Janne <jannekarelia@gmail.com>
2015-02-13 00:41:48 +0200

    Reading questions from database and showing them on client side

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ var MongoClient = require('mongodb').MongoClient
 var bodyParser = require('body-parser');
 
 
+
 MongoClient.connect('mongodb://localhost:27017/anniskelupassi', function (err, db) {
 	if (err) {
 	    throw err;
 	} else {
 	    console.log("Successfully connected to the database ");
+	    var collection = db.collection('anniskelupassi');
 	}
 
 	app.use(bodyParser.json());
 
 	app.use(express.static(__dirname + '/public'));
+
+	/* GET /haekysymykset */
+	/* Hakee kysymykset tietokannalta ja lähettää ne clientille? Yes? No? */
+	app.get("/haekysymykset", function(req, res) {
+		  collection.find().toArray(function (err, items) {
+	    		res.send(items);   						
+  			});
+	});
+
+	
 
 	var server = app.listen(3000, function () {
 		var host = server.address().address
---
+++ public/js/functions.js
@@ -0,0 +1,26 @@
+/* Hakee kysymykset tietokannasta */
+function FetchQuestions() {
+	$.ajax ({
+		url : "haekysymykset",
+		dataType :"json",
+		type:"GET",
+
+		/* Jos haku onnistui, siirrytään tähän lohkoon. */
+		/* Success parametri data pitää sisällään haun tulokset array muodossa. */
+		success : function(data) {
+			PrintAllQuestions(data);
+		},
+
+		error : function(err) {
+			console.log(err);				
+		}
+	});
+}
+
+/* Tulostetaan HTML dokumenttiin FetchQuestions() funktion löytämät rivit */
+/* Placeholder, saa muuttaa ja tulee muuttumaan. */
+function PrintAllQuestions(jsonData) {
+	for (var i = 0; i < jsonData.length; i++) {
+		$("#kysymykset").append("<p>" + jsonData[i].Question + "</p>");					
+	}
+}

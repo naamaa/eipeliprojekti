367320d31569c03abc0875b79d7305d57b41b1da
Pekka Ikonen <pekka.ikonen@gmail.com>
2015-03-29 13:44:28 +0300

    Footer modifications.
    Added numeric timer with submit in 60mins. Added question counter.
    Moved submit button to footer and added submit function to button.

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,7 @@
 // Function for initializing exam.html
 function initExam() {
 	fetchExamQuestions();
-	showTimerBar();
+	startTimer();
 }
 
 // Sends request for getting the questions from database
@@ -25,6 +25,8 @@ function fetchExamQuestions() {
 
 // Prints the fetched questions with the true/false options
 function printExamQuestions(jsonData) {
+	var counterString = "0/16";
+	document.getElementById("counter").innerHTML = counterString;
 	var questionCounter = 1;
 	for (var i = 0; i < jsonData.length; i++) {
 		var answerCounter = 0;
@@ -53,7 +55,17 @@ function printExamQuestions(jsonData) {
 			+ '" value="false" type="radio">Väärin'  
 			+ '</input></label>');
 
-		questionCounter++;				
+		questionCounter++;
+
+		// function to count the number of selected radio inputs
+		// number displayed in the footer
+		$(document).ready(function(){
+			$('input:radio').change(function(){
+				var checkedRadio = $('input:radio:checked').length;
+				counterString = checkedRadio+"/16";
+				document.getElementById("counter").innerHTML = counterString;
+			})
+		});	
 	}
 }
 
@@ -74,6 +86,39 @@ function getUrlParameter(sParam) {
     }
 }
 
+// numeric timer displayed in the footer
+function startTimer() {
+	var s = 0;
+	var m = 0;
+	var t;
+	count();
+	function count() {
+    	s = s + 1;
+    	t = setTimeout(function(){ count() }, 1000);
+    	if(s == 60) { s = 0; m = m + 1; }
+    	time = (m < 10 ? "0" + m : m) + ":" + (s < 10 ? "0" + s : s);
+    	document.getElementById("timer").innerHTML = time;
+    	if (m == 60) {
+    		submitform()
+    	}
+	}
+}
+
+// submit form function called from the submit button
+function submitform()
+{
+  document.answerform.submit();
+}
+
+// Toggle visibility of admin login elements
+function toggleAdminLogin() {
+	if (document.getElementById("admin-login").style.display == "block")
+		document.getElementById("admin-login").style.display = "none";
+	else if (document.getElementById("admin-login").style.display == "none")
+		document.getElementById("admin-login").style.display = "block";
+}
+
+/*
 // Progress bar for exam.html
 function showTimerBar(callback) {
     var bar = document.getElementById('progress'),
@@ -88,11 +133,4 @@ function showTimerBar(callback) {
         }
     }, 1000);
 }
-
-// Toggle visibility of admin login elements
-function toggleAdminLogin() {
-	if (document.getElementById("admin-login").style.display == "block")
-		document.getElementById("admin-login").style.display = "none";
-	else if (document.getElementById("admin-login").style.display == "none")
-		document.getElementById("admin-login").style.display = "block";
-}
+*/

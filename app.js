bd71e632dbb8d4e77450703131fb16bfc08237c4
ryger <vartiainen.henri@gmail.com>
2015-04-09 10:29:29 +0300

    Made login to work with new system

--- app.js
+++ app.js
@@ -60,8 +60,9 @@ MongoClient.connect('mongodb://localhost:27017/anniskelupassi', function (err, d
 	//Passport strategy for logincodes
 	passport.use(new LocalAPIKeyStrategy(
  		function(apikey, done) {
- 		 	var User = db.collection('logincodes');
-    		User.findOne({ logincode: apikey }, function (err, user) {
+ 			console.log(apikey);
+ 		 	var User = db.collection('exams');
+    		User.findOne({loginid: apikey}, function (err, user) {
      			if (err) {
      			 return done(err); 
      			}
@@ -162,6 +163,63 @@ MongoClient.connect('mongodb://localhost:27017/anniskelupassi', function (err, d
 										failureRedirect: '/?retry=true'})
 	); 
 
+/* POST/create_exam  creates unique cutting edge login code for user and inserts it to database. Also administrator name must be given and starttime */
+
+	app.post('/create_exam', function(req,res){
+
+			autoIncrement.getNextSequence(db, "exams", function(err, autoIndex){
+			var exams = db.collection('exams');
+			var id = autoIndex;
+
+			//Creates login code and ensures it doesnt do duplicaters
+
+			var create_logincode = function(){
+
+				var logincode = Math.floor(Math.random() * 9000) + 1000;    		
+				var found = false;
+				exams.count({ loginid: logincode }, function (err, count) {
+     			if(count > 1){
+     				found = true;
+     			}
+     			else{
+     				found = false;
+     			}
+    		});
+			if(found){
+				create_logincode();
+			}
+			if(!found){
+				return logincode;
+			}
+			};
+
+			var loginid = create_logincode().toString();
+			console.log("LoginID:" + loginid);
+			console.log("Adding exam with ID : " + autoIndex);
+			exams.insert( {
+				_id: id,
+				loginid: loginid,
+				admin: req.body.admin,
+				starttime: req.body.starttime,
+				endtime: "false",
+				students:  ""
+			}, function(err, result) {
+				if (err) {
+					console.log(err);
+					res.json({succesful: false});
+				}
+				else if (result) {
+					console.log("Added!");
+					res.json({succesful: true});
+				}
+				else if (!result) {
+					console.log("Couldn't add exam.")
+					res.json({succesful: false});
+				}
+			});
+		});
+	});
+	
 	// Gets questions and id's from database (answer is not sent to client)
 	app.get("/get_questions", function(req, res) {
 		var questions = db.collection('questions');
@@ -186,16 +244,6 @@ MongoClient.connect('mongodb://localhost:27017/anniskelupassi', function (err, d
 	  	questions.find().toArray(function (err, items) {
 	  		console.log("Sending all question data to client (ADMIN), I'll let you know if something goes wrong.");
     		res.send(items);   						
-		});
-	});
-
-	// Gets all question data from database 
-	app.get("/get_exams", function(req, res) {
-		var exams = db.collection('exams');
-
-	  	exams.find().toArray(function (err, items) {
-	  		console.log("Sending all exam data to client (ADMIN), I'll let you know if something goes wrong.");
-    		res.send(items);		
 		});
 	});
 
